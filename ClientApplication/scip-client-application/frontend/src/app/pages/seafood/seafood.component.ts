bb23bd701c2a22e4ac2b1af9c4ca377def64cec3
lampajr <lampa9559@gmail.com>
2020-01-09 16:03:01 +0100

    forms and registering opreation implemented

--- ClientApplication/scip-client-application/frontend/src/app/pages/seafood/seafood.component.ts
+++ ClientApplication/scip-client-application/frontend/src/app/pages/seafood/seafood.component.ts
@@ -1,5 +1,5 @@
 import {COMMA, ENTER} from '@angular/cdk/keycodes';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ApiService } from 'src/app/api.service';
 import { MatChipInputEvent } from '@angular/material/chips';
@@ -11,6 +11,7 @@ import { MatChipInputEvent } from '@angular/material/chips';
 })
 export class SeafoodComponent implements OnInit {
   title = 'Seafood Supply Chain';
+
   fishForm: FormGroup;
   packageForm: FormGroup;
   shipmentForm: FormGroup;
@@ -18,11 +19,16 @@ export class SeafoodComponent implements OnInit {
   sellingForm: FormGroup;
   transportationForm: FormGroup;
 
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
   idsForPackage: string[] = [];
+  idsForShipment: string[] = [];
   selectable = true;
   removable = true;
   addOnBlur = true;
-  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+
+  performing = false;
+  result: any = null;
+  prevIndex = 0;
 
   constructor(private apiService: ApiService, private formBuilder: FormBuilder) {
     this.fishForm = this.formBuilder.group({
@@ -31,16 +37,19 @@ export class SeafoodComponent implements OnInit {
       fishermanName: '',
     });
     this.packageForm = this.formBuilder.group({
-      fishdIds: this.formBuilder.array([this.idsForPackage]),
+      fishIds: [this.idsForPackage],
       packageId: '',
       processingFacilityName: '',
     });
+    this.packageForm.controls.fishIds.setValue(this.idsForPackage);
 
     this.shipmentForm = this.formBuilder.group({
       fishIds: '',
       toLocation: '',
       shipmentCompanyName: '',
     });
+    this.shipmentForm.controls.fishIds.setValue(this.idsForShipment);
+
     this.transportationForm = this.formBuilder.group({
       packageId: '',
       toLocation: '',
@@ -59,54 +68,147 @@ export class SeafoodComponent implements OnInit {
   }
 
   onRegisterFish(fish: any) {
-    console.log(fish);
+    this.performing = true;
+    this.apiService.registerCapturedFish(fish)
+      .subscribe(
+        res => {
+          this.result = res;
+          this.performing = false;
+        },
+        err => {
+          this.result = err.message;
+          this.performing = false;
+      });
     this.fishForm.reset();
   }
 
   onRegisterPackage(packageFish: any) {
-    console.log(packageFish);
+    this.performing = true;
+    this.apiService.registerPackage(packageFish)
+      .subscribe(
+        res => {
+          this.result = res;
+          this.performing = false;
+        },
+        err => {
+          this.result = err.message;
+          this.performing = false;
+      });
     this.packageForm.reset();
   }
 
   onRegisterShipment(shipment: any) {
-    console.log(shipment);
+    this.performing = true;
+    this.apiService.registerShipment(shipment)
+      .subscribe(
+        res => {
+          this.result = res;
+          this.performing = false;
+        },
+        err => {
+          this.result = err.message;
+          this.performing = false;
+      });
     this.shipmentForm.reset();
   }
 
   onRegisterTransportation(transportation: any) {
-    console.log(transportation);
+    this.performing = true;
+    this.apiService.registerTransportation(transportation)
+      .subscribe(
+        res => {
+          this.result = res;
+          this.performing = false;
+        },
+        err => {
+          this.result = err.message;
+          this.performing = false;
+      });
     this.transportationForm.reset();
   }
 
   onRegisterInInventory(entry: any) {
-    console.log(entry);
+    this.performing = true;
+    this.apiService.registerInInventory(entry)
+      .subscribe(
+        res => {
+          this.result = res;
+          this.performing = false;
+        },
+        err => {
+          this.result = err.message;
+          this.performing = false;
+      });
     this.inventoryEntryForm.reset();
   }
 
   onRegisterSelling(sale: any) {
-    console.log(sale);
+    this.performing = true;
+    this.apiService.registerSelling(sale)
+      .subscribe(
+        res => {
+          this.result = res;
+          this.performing = false;
+        },
+        err => {
+          this.result = err.message;
+          this.performing = false;
+      });
     this.sellingForm.reset();
   }
 
   removeIdFromPackage(id: string) {
+    const controller = this.packageForm.controls.fishIds;
     const index = this.idsForPackage.indexOf(id);
     if (index >= 0) {
       this.idsForPackage.splice(index, 1);
     }
+    controller.markAsDirty();
   }
 
   addIdInPackage(event: MatChipInputEvent): void {
     const input = event.input;
     const value = event.value;
 
-    // Add our fruit
+    // Add a new id
     if ((value || '').trim()) {
       this.idsForPackage.push(value);
-    }
 
-    // Reset the input value
-    if (input) {
+      this.packageForm.controls.fishIds.markAsDirty();
+      // Reset the input value
       input.value = '';
+    }
+  }
+
+  removeIdFromShipment(id: string) {
+    const controller = this.shipmentForm.controls.fishIds;
+    const index = this.idsForShipment.indexOf(id);
+    if (index >= 0) {
+      this.idsForShipment.splice(index, 1);
+    }
+    controller.markAsDirty();
+  }
+
+  addIdInShipment(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    // Add a new id
+    if ((value || '').trim()) {
+      this.idsForShipment.push(value);
+
+      this.shipmentForm.controls.fishIds.markAsDirty();
+      // Reset the input value
+      input.value = '';
+    }
+  }
+
+  onTabClick(event) {
+    if (event.index !== this.prevIndex) {
+      console.log('changing tab to ' + event.index);
+      this.result = null;
+      this.performing = false;
+      this.prevIndex = event.index;
     }
   }
 }
