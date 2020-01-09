b670c8f9aa1b52bc6225eb19d7aee1f13226821c
lampajr <lampa9559@gmail.com>
2020-01-09 22:47:16 +0100

    index page implemented

--- ClientApplication/scip-client-application/frontend/src/app/pages/index/index.component.ts
+++ ClientApplication/scip-client-application/frontend/src/app/pages/index/index.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-index',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  toSeafood() {
+    this.router.navigate(['/seafood']);
+  }
+
+  toDairy() {
+    this.router.navigate(['/dairy']);
+  }
 }
