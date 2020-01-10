88df704c95cf5524be77d8998568cebcda9a7cf5
lampajr <lampa9559@gmail.com>
2020-01-10 16:07:11 +0100

    removed all files related to the previous case study

--- ClientApplication/scip-client-application/frontend/src/app/EmsState.ts
+++
@@ -1,8 +0,0 @@
-export class EmsState {
-  powerProviderPrice: string;
-  powerProviderAmount: string;
-  powerProviderBalanceEur: string;
-  powerPlantPrice: string;
-  powerPlantAmount: string;
-  powerPlantBalanceEur: string;
-}
